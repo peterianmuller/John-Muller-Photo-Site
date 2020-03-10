9501d1ef5dcae749580bfa932407eef8d017eea2
peterianmuller <peterianmuller@gmail.com>
2020-03-10 12:57:52 -0700

    Initial commit -> re-created git repo to hide db config info

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const { DATABASE_URL, dbInfo } = require("dbInfo/dbConfig");
+
+var pool = mysql.createPool(dbInfo);
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM photos", function(error, results, fields) {
+    if (error) throw error;
+    res.render("photos.ejs", { photos: results });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
